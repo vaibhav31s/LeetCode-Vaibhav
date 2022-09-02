a4f32a2e3cf33db032a975dd01c10a37a5be7c19
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-09-02 11:13:56 +0530

    Time: 11 ms (5.70%), Space: 48.6 MB (10.54%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -16,24 +16,26 @@
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
         List<Double> answer = new ArrayList<>();
-        Deque<TreeNode> dq= new LinkedList<>();
-        dq.add(root);
-        while(!dq.isEmpty()){
-            int size = dq.size();
-            double add = 0;
-            double len = size;
-            while(size--> 0 ){
-                TreeNode temp = dq.poll();
-                if(temp.left != null) dq.add(temp.left);
-                if(temp.right != null) dq.add(temp.right);
-                add+= temp.val;
+        HashMap<Integer, List<Integer>> levels = new LinkedHashMap<>();
+        dfs(root, levels, 0);
+        
+        for(int key : levels.keySet()){
+            List<Integer> list = levels.get(key);
+            double a =  0;
+            double len = list.size();
+            for(int x : list){
+                a +=x;
             }
-            
-            answer.add(add/len);
-            
+            answer.add(a/len);
         }
         
-        
         return answer;
+    }
+    void dfs(TreeNode root, HashMap<Integer, List<Integer>> levels, int level){
+        if(root== null) return;
+        levels.putIfAbsent(level, new ArrayList<>());
+        levels.get(level).add(root.val);
+        dfs(root.left, levels, level+1);
+        dfs(root.right, levels, level+1);
     }
 }
