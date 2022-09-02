5f81131c018e9dcf20189f322c90c4855015c9f0
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-09-02 13:13:30 +0530

    Time: 4 ms (70.59%), Space: 55.4 MB (34.16%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -3,20 +3,15 @@ class Solution {
         List<List<Integer>> answer = new ArrayList<>();
         boolean[] vis = new boolean[graph.length];
             List<Integer> list = new ArrayList<>();
-        list.add(0);
-        for(int x : graph[0]){
-                vis[0] =true;
-                list.add(x);
-                dfs(graph, vis, answer, list, x);
-                list.remove(list.size()-1);
-                vis[0] = false;
-        }
-        
+                list.add(0);
+                dfs(graph, vis, answer, list, 0);
+               
+       
         return answer;
     }
     
     void dfs(int[][] graph, boolean[] vis, List<List<Integer>> answer, List<Integer> list, int node){
-        if(list.get(list.size()-1) == graph.length- 1){
+        if(list.size() != 0 && list.get(list.size()-1) == graph.length- 1){
             answer.add(new ArrayList<>(list));
         }
         if(!vis[node]){
