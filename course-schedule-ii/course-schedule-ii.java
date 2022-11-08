9b5b03a2b2a82a7a062ff9ea2722b85b46a0872d
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-11-08 17:09:46 +0530

    Time: 373 ms, Memory: 119.2 MB - LeetHub

---
+++ course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] findOrder(int n, int[][] relations) {
+        int[] indegree = new int[n];
+        int[] answer = new int[n];
+        int count =0;
+        HashMap<Integer, HashSet<Integer>> alist=  new HashMap<>();
+        for(int i = 0; i < n; i++){
+            alist.put(i,new HashSet<>());
+        }
+        
+        for(int[] x : relations){
+            alist.get(x[0]).add(x[1]);
+        }
+        
+        for(int i = 0 ; i < n; i++){
+            indegree[i] =  alist.get(i).size();
+        }
+        
+                System.out.println(alist);
+
+        System.out.println(Arrays.toString(indegree));
+        
+        Deque<Integer> dq =  new LinkedList<>();
+        
+        
+        do{
+           
+         
+            int nn = dq.size();
+            while(nn-- > 0){
+                int a = dq.poll();
+                indegree[a]--;
+                for(int i = 0; i < n; i++){
+                    if(alist.get(i).contains(a)) indegree[i]--;
+                }
+            answer[count] =  a;
+                count++;
+            }
+            
+            for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){ dq.add(i);
+            }
+              
+        }
+            if(dq.size() == 0 && count != n) return new int[0];
+                
+        
+        }while(!dq.isEmpty());
+        
+        return answer;
+    }
+}
