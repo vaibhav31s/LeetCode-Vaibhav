d5a5bb7d48c96c555f4c27530f11d61d5460606a
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-11-13 21:04:03 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (16.37%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    int[] dp = new int[46];
+    public int climbStairs(int n) {
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(dp[n] != 0) return dp[n];
+        
+        return dp[n] = climbStairs(n - 1) +  climbStairs(n -2); 
+    }
+}
