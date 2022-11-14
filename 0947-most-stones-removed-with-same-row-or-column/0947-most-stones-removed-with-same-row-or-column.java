988bc642e4aa13225e9ef3b24970e12fcb8783a1
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-11-15 00:32:15 +0530

    Time: 28 ms (80.41%), Space: 65.4 MB (5.89%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -13,29 +13,45 @@ class Solution {
         return stones.length - set.size();
     }
 }
-class UnionFind{
-    int[] root;
-    int[] rank;
-    UnionFind(int size){
-        root = new int[size+1];
-       
-        for(int i = 0; i < size; i++){
+
+class UnionFind {
+     int[] root;
+     int[] rank;
+
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
             root[i] = i;
-   
+            rank[i] = 1; 
         }
     }
-    int find(int x){
-        if(x == root[x]) return x;
+
+
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
         return root[x] = find(root[x]);
     }
-    
-    void union(int x, int y){
+
+
+    public void union(int x, int y) {
         int rootX = find(x);
         int rootY = find(y);
-        for(int i =0; i < root.length; i++){
-                    if(rootY == root[i]){
-                            root[i] = rootX;
-                    }
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
         }
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
     }
 }
