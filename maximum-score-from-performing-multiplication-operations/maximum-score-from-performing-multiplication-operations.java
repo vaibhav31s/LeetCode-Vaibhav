9e8b39437fa23f592631efca5744dfce11c79517
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-11-17 12:20:56 +0530

    Time: 79 ms, Memory: 202.3 MB - LeetHub

--- maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.java
@@ -1,24 +1,23 @@
 class Solution {
     
-    HashMap<String, Integer> map = new HashMap<>();
+    
     public int maximumScore(int[] nums, int[] multipliers) {
-        int[][] dp = new int[ multipliers.length + 1][ nums.length + 1];
+        Integer[][] dp = new Integer[ multipliers.length + 1][nums.length + 1];
         
         return helper(nums, multipliers, 0,0,nums.length-1,dp);
     }
     
-    int helper(int[] nums, int[] mul, int index, int nstart, int nend,int[][] dp){
+    int helper(int[] nums, int[] mul, int index, int nstart, int nend,Integer[][] dp){
         
         if(nstart > nend) return 0;
         if(index >= mul.length) return 0;
-        
-        String key = index + "," + nstart +"," + nend;
-        if(map.containsKey(key)) return map.get(key);
+      if ( dp[index][nend] != null) return  dp[index][nend];
+        // if(map.containsKey(key)) return map.get(key);
         int left = helper(nums, mul, index+1, nstart + 1, nend,dp) +  (mul[index] * nums[nstart]);
         int right =  helper(nums, mul, index + 1, nstart, nend - 1,dp) +  (mul[index] * nums[nend]);
         
-        map.put(key,Math.max(left, right));
-        return Math.max(left, right);
+      
+        return  dp[index][nend] = Math.max(left, right);
         
     }
 }
