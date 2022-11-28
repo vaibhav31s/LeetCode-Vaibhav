b662a335f811ca80b613ad789de2f223111e79ce
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-11-28 21:58:25 +0530

    Time: 132 ms (17.70%), Space: 42.7 MB (89.45%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        Integer[] dp = new Integer[nums.length + 1];
+        return dfs(nums, 0,dp);
+    }
+    
+    int dfs(int[] nums, int index, Integer[] dp){
+        // int len = nums.length;
+        if(index == nums.length - 1 ) return 0;
+        if(dp[index] != null) return dp[index];
+        int max = Math.min(nums.length - 1, index + nums[index]);
+        
+        int min = 500000;
+        for(int i = max; i > index; i--){
+            min = Math.min(1 + dfs(nums, i, dp),min);
+        } 
+        
+        
+        return dp[index] = min;
+    }
+}
