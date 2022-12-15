82ee8d187a29ee604e9fead04e939a8b9ae05de5
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-12-15 11:33:01 +0530

    Time: 16 ms (82.71%), Space: 53.1 MB (7.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,28 +1,15 @@
 class Solution {
     public int longestCommonSubsequence(String a, String b) {
-        int[][] dp = new int[a.length() + 1][ b.length()  + 1];
-        
-        dp[a.length()][b.length()] = 0;
-        
-        for(int i  = a.length() - 1; i >= 0; i--){
-            for(int j = b.length() - 1; j >= 0 ; j--){
-                if(a.charAt(i) == b.charAt(j)){
-                    dp[i][j] = 1 + dp[i+1][j+1];
-                }else{
-                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
-                }
-            }
-        }
-        
-        
-       return dp[0][0];
+        Integer[][] dp = new Integer[a.length() + 1][b.length() + 1];
+        return helper(a.toCharArray(), b.toCharArray(),0,0,dp);
     }
-    
-    int helper(String a, String b, int i , int j,int[][] dp){
+    int helper(char[] a, char[] b, int i, int j,Integer[][] dp){
+        if(i >= a.length || j >= b.length ) return 0;
+        if(dp[i][j] != null) return dp[i][j];
+        if(a[i] ==  b[j]){
+            return dp[i][j] =  1 +  helper(a, b, i + 1, j + 1,dp);
+        }
+        return dp[i][j]=  Math.max(helper(a, b, i + 1, j,dp ),helper(a, b, i , j + 1,dp)) ;  
         
-        if(i == a.length()|| j == b.length()) return 0;
-        if(dp[i][j] != 0) return dp[i][j];
-        if(a.charAt(i) == b.charAt(j)) return 1 + (helper(a, b, i+1, j+1,dp));
-        return  dp[i][j] = Math.max(helper(a, b, i + 1, j,dp),(helper(a, b, i , j + 1,dp)));
     }
 }
