5d76967b7230a099abdd8936fe6fa79df2b00a9a
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-12-18 07:41:28 +0530

    Time: 239 ms (13.46%), Space: 129.9 MB (73.73%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,18 +1,28 @@
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
-        Stack<Pair<Integer, Integer>> stack = new Stack<>();
-        int[] answer = new int[temp.length];
-        for(int i =0 ; i < temp.length; i++){
-           
-                while(!stack.isEmpty() && stack.peek().getKey() < temp[i]){
-                    int index = stack.pop().getValue();
-                    answer[index] =  i - index;
+        
+        Stack<Pair<Integer,Integer>> stack = new Stack<>();
+        
+        for(int i = 0; i < temp.length; i++){
+            if(stack.isEmpty()){
+                stack.push(new Pair(i, temp[i]));
+            }else if(stack.peek().getValue() > temp[i]){
+                stack.push(new Pair(i, temp[i]));
+            }else{
+                
+                while(!stack.isEmpty() && stack.peek().getValue() < temp[i]){
+                    Pair<Integer,Integer> tempp = stack.pop();
+                    temp[tempp.getKey()] =i -  tempp.getKey();
                 }
-                stack.push(new Pair(temp[i],i));
+                
+                stack.push(new Pair(i, temp[i]));
             }
+        }
         
-        // while(!stack.isEmpty()){}
-        
-        return answer;
+        while(!stack.isEmpty()){
+                    Pair<Integer,Integer> tempp = stack.pop();
+                    temp[tempp.getKey()] =0;
+        }
+        return temp;
     }
 }
