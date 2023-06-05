0c1f4206412a7ddd566532d6d8a24fa8d81f5f37
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-06-05 18:03:58 +0530

    Time: 23 ms (5.91%), Space: 41.7 MB (25.53%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,29 +1,30 @@
 class Solution {
     public int shortestWay(String source, String target) {
-        int i = 0;
-        int j = 0;
-        Set<Character> set = new HashSet<>();
-        // Set<Integer> set1 = new HashSet<Character>();
-        // f
-        for(char ch : source.toCharArray()) set.add(ch);
+        //Using Set to Check Whether source contains all the targets chars //Base case
+        Set<Character> checker = new HashSet<>();     
+        for(char ch : source.toCharArray()) checker.add(ch);
         for(char ch : target.toCharArray()){
-            if(!set.contains(ch)) return -1;
+            if(!checker.contains(ch)) return -1;
         }
-        int answer = 0;
-
-        while(j < target.length()){
-            String s = target.substring(i,j+1);
-            if(isSubsequence(s,source)){
-                
-            }else{
-                i = j;
-                answer++;
-            } 
-             j++;
-        }
-        // isSubsequence(s,source) answer++;
-        return answer+1;
         
+        
+        
+        int answer = 1;
+        int p1 = 0;
+        int p2 = 0;
+    
+        //bruteforce checks all the possible p1 to p2 from target is subsequnce of source
+        while(p1 < target.length()){
+            if(!isSubsequence(target.substring(p2, p1+1), source)){
+                answer++;
+                p2 = p1;
+            }
+            p1++;
+        }
+        
+        
+        
+        return answer;
     }
       public boolean isSubsequence(String s, String t) {
         
