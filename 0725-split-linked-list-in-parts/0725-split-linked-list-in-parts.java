39c2473ef019710d3a7047967b4ad33fb129f036
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-09-06 15:31:01 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (63.92%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int n = getTotalNodes(head);
+        ListNode[] answer = new ListNode[k];
+        if (n <= k) {
+            for (int i = 0; i < k; i++) {
+                if(head == null) {
+                     answer[i] = null;
+                    continue;
+                }
+                answer[i] = head;
+                head = head.next;
+            }
+            for (int i = 0; i < k; i++) {
+                if(answer[i] == null) continue;
+                answer[i].next = null;
+            }
+           
+            return answer;
+        }
+         
+        int total = n / k; 
+        int i = 0;
+       
+        while ( i < answer.length) {
+            boolean flag = n % k != 0 ? false : true;
+            
+            answer[i] = head;
+            ListNode last = head;
+            total = n / k;
+            if (!flag) {
+              
+               total++;
+        
+            }
+            n -= total;
+            k--;
+            for(int j = 0; j < total; j++) {
+                if(head == null) continue;
+                last = head;
+                head = head.next;
+            }
+            if(last != null) {
+                last.next = null;
+            }
+            
+            
+            i++;
+        }
+        if(i < answer.length)
+            answer[i] = head;    
+        return answer;
+        
+    }
+    
+    
+    int getTotalNodes(ListNode head) {
+        int cnt = 0;
+        while (head != null) {
+            head = head.next;
+            cnt++;
+        }
+        return cnt;
+    }
+}
