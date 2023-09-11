1c1f5805a638b86031c74390658513fd110ad082
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-09-12 00:59:07 +0530

    Time: 5 ms (93.77%), Space: 44.3 MB (70.67%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,23 +1,19 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
-        for(int i = 0; i < groupSizes.length; i++) {
-            int x = groupSizes[i];
-            map.putIfAbsent(x, new ArrayList<>());
-            map.get(x).add(i);
-        }
-        
+        Map<Integer, List<Integer>> map = new HashMap<>();
         List<List<Integer>> answer = new ArrayList<>();
-        for(int x : map.keySet()) {
-            List<Integer> list = map.get(x);
-            for(int i = 0; i < list.size(); ) {
-            List<Integer> temp = new ArrayList<>();
-                for(int j = 0; j < x && i < list.size(); i++, j++) {
-                    temp.add(list.get(i));
-                }
-            answer.add(temp);
-            }
+
+        for (int i = 0; i < groupSizes.length; i++) {
+            int x = groupSizes[i];
+            map.computeIfAbsent(x, k -> new ArrayList<>()).add(i);
         }
+
+        map.forEach((k, v) -> {
+            for (int i = 0; i < v.size(); i += k) {
+                answer.add(v.subList(i, i + k));
+            }
+        });
+
         return answer;
     }
 }
