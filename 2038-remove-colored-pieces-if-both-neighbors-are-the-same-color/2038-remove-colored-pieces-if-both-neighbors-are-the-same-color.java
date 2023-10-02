2c7a9a94ed1452fd447d982b147170e6bbb9b6e7
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-10-02 06:12:54 +0530

    Time: 8 ms (93.26%), Space: 44.3 MB (30.02%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,27 +1,19 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
         int alice = 0, bob = 0, countA = 0, countB = 0;
-        for(char ch : colors.toCharArray()) {
-            if(ch == 'A') {
-                if(countA == 0) 
-                     bob += Math.max(0, countB - 2);
-                
+        for (char ch : colors.toCharArray()) {
+            if (ch == 'A') {
                 countA++;
                 countB = 0;
             } else {
-                if(countB == 0) 
-                    alice += Math.max(0, countA - 2);
-                
                 countB++;
                 countA = 0;
             }
+
+            alice += (countA >= 3) ? 1 : 0;
+            bob += (countB >= 3) ? 1 : 0;
         }
-        if(countA == 0)
-            bob += Math.max(0, countB - 2);
-        
-        if(countB == 0)
-            alice += Math.max(0, countA - 2);
-        
+
         return alice > bob;
     }
 }
