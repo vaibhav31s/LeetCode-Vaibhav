afa3c742eb542bbea6e3cddd0290bf0c8bd6375f
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-10-02 06:11:35 +0530

    Time: 8 ms (93.26%), Space: 44.1 MB (68.76%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,44 +1,28 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
-        ArrayList<Pair<Character, Integer>> list = new ArrayList<>();
-        int countA = 0, countB = 0;
-        char last = '-';
+        int alice = 0, bob = 0, countA = 0, countB = 0;
         for(char ch : colors.toCharArray()) {
             if(ch == 'A') {
-                if(countA == 0) {
-                    list.add(new Pair('B', countB));
-                }
+                if(countA == 0) 
+                     bob += Math.max(0, countB - 2);
+                
                 countA++;
                 countB = 0;
             } else {
-                if(countB == 0) {
-                    list.add(new Pair('A', countA));
-                }
+                if(countB == 0) 
+                    alice += Math.max(0, countA - 2);
+                
                 countB++;
                 countA = 0;
             }
         }
-        if(countA == 0) {
-            list.add(new Pair('B', countB));
-        }
-        if(countB == 0) {
-            list.add(new Pair('A', countA));
-        }
-        int alice = 0, bob = 0;
+        if(countA == 0)
+            bob += Math.max(0, countB - 2);
         
-        for(Pair<Character,Integer> p : list) {
-            int val = (p.getValue() - 2);
-            if(p.getKey().equals('A')) {
-                alice += Math.max(0, val);
-            } else {
-                bob += Math.max(0, val);
-            }
-        }
-        
-        // System.out.println(alice + " " + bob);
+        if(countB == 0)
+            alice += Math.max(0, countA - 2);
         
         return alice > bob;
-        
     }
 }
 
