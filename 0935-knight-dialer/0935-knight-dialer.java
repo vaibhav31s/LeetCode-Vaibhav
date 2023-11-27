fd5e8de542fd5f90bfdb475e47ea0b408fb26de8
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-11-27 09:08:33 +0530

    Time: 31 ms (65.76%), Space: 43 MB (80.71%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,17 +1,37 @@
 class Solution {
     static long mod = 1000000007;
     public int knightDialer(int n) {
-        Integer[][] dp = new Integer[n + 2][10];
-
+        int[][] dp = new int[n][10];
+        Arrays.fill(dp[0], 1);
         // int[] moves = new int[]{2,2,2,2,3,0,3,2,2,2};
         int[][] canGo = {new int[]{4,6},new int[]{6, 8}, new int[]{7,9}, new int[]{4,8}, new int[]{3,9, 0}, new int[] {}, new int[]{0,1,7}, new int[]{2, 6}, new int[]{1,3}, new int[]{2,4}};
       
         int ans = 0;
        
-        for(int i = 0; i <= 9; i++) {
-            ans += go(canGo, n, i, dp);
+        // for(int i = 0; i <= 9; i++) {
+        //     ans += go(canGo, n, i, dp);
+        //     ans %= mod;
+        // }
+        for (int i = 1; i < n; i++) {
+           
+            for(int cur = 0; cur < 10; cur++) {
+            
+                for(int j = 0; j < canGo[cur].length; j++) {
+                    
+                        dp[i][cur] += dp[i - 1][canGo[cur][j]];
+                        dp[i][cur] %= mod;
+                    
+                }
+              
+            }
+        }
+        
+        // int ans = 0;
+        for(int x :  dp[n - 1])  {
+            ans += x;
             ans %= mod;
         }
+        // System.out.println(Arrays.toString(dp[n]));
         return ans;
             
             
