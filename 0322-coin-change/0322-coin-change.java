026421e8f7b136c145bd008761a8ebff12e934dc
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-11-29 21:45:06 +0530

    Time: 21 ms (38.06%), Space: 43.8 MB (24.38%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -3,29 +3,34 @@ class Solution {
      
         long[] dp = new long[amount + 1];
         
-        int answer = (int) possible(coins, amount, dp);
+        // int answer = (int) possible(coins, amount, dp);
         
-        return answer == Integer.MAX_VALUE ? -1 :  answer - 1 ;
-    } 
-    
-    long possible(int[] coins, int amount, long[] dp) {
-        if(amount == 0) return 1;
-        
-        if(amount < 0) return Integer.MAX_VALUE;
-        
-        if(dp[amount] != 0) return dp[amount];
-        
-        long minWay = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            
-            long way = possible(coins, amount - coin, dp);
-            if(way == Integer.MAX_VALUE) continue;
-            minWay = Math.min(1 + way, minWay);
+        Arrays.fill(dp,  Integer.MAX_VALUE);
+        dp[0] = 1;
+        for (int remainAmount = 1; remainAmount <= amount; remainAmount++) {
+             
+                long minWay = Integer.MAX_VALUE;
+                for (int coin : coins) {
+                    if(remainAmount - coin < 0){ 
+                        continue;
+                    }
+                    long way = dp[remainAmount - coin];
+                    
+                    if(way == Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    
+                    
+                    minWay = Math.min(1 + way, minWay);
+                }
+
+             dp[remainAmount] = minWay;
         }
         
-        return dp[amount] = minWay;
-    }
+        return  dp[amount] == Integer.MAX_VALUE ? -1 : (int) dp[amount] - 1 ;
+    } 
     
+   
     
 }
 
