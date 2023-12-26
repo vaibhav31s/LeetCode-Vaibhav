9592fbee89dc143a891ba43600716601bb65bc9a
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-12-26 18:24:23 +0530

    Time: 30 ms (10.32%), Space: 40.3 MB (88.08%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,17 +1,14 @@
 class Solution {
-     long mod = 1000000007;
+     int mod = 1000000007;
     public int numRollsToTarget(int n, int k, int target) {
-        long[] lastState = new long[target + 1];
-        long[] curState = new long[target + 1];
+        int[] lastState = new int[target + 1];
+        int[] curState = new int[target + 1];
         lastState[0] = 1;
         for(int nn = 1; nn <= n; nn++) {
-            
             for(int targett = 0;  targett <= target; targett++) {
-
                 for(int i = 1; i <= k; i++) {
                     if((targett - i) < 0) break;
-                    curState[targett] += lastState[targett - i];
-                    curState[targett] %= mod;
+                    curState[targett] = (curState[targett] + lastState[targett - i]) % mod;
                 }   
              
             }
@@ -21,7 +18,7 @@ class Solution {
                 curState[i] = 0;
             }
         }
-       return (int) lastState[target]; 
+       return lastState[target]; 
     }
   
 }
