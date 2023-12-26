79317261acf6efd4162915f3d9848bf0383c3e07
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-12-26 18:21:41 +0530

    Time: 25 ms (13.88%), Space: 40.4 MB (82.03%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,38 +1,27 @@
 class Solution {
      long mod = 1000000007;
     public int numRollsToTarget(int n, int k, int target) {
-        long[][] dp = new long[n + 1][target + 1];
-     
-        dp[0][0] = 1;
+        long[] lastState = new long[target + 1];
+        long[] curState = new long[target + 1];
+        lastState[0] = 1;
         for(int nn = 1; nn <= n; nn++) {
+            
             for(int targett = 0;  targett <= target; targett++) {
 
                 for(int i = 1; i <= k; i++) {
                     if((targett - i) < 0) break;
-                    dp[nn][targett] += dp[nn - 1][targett - i];
-                    dp[nn][targett] %= mod;
+                    curState[targett] += lastState[targett - i];
+                    curState[targett] %= mod;
                 }   
              
             }
-         
+            
+            for(int i= 0; i < lastState.length; i++) {
+                lastState[i] = curState[i];
+                curState[i] = 0;
+            }
         }
-       return (int) dp[n][target]; 
+       return (int) lastState[target]; 
     }
-    long go(int n, int k, int target, long[][] dp) {
-        if(target == 0 && n == 0) {
-            return 1;
-        }
-        
-        
-        if(target < 0 || n < 0) return 0;
-        
-        if(dp[n][target] != -1) return dp[n][target];
-        long count = 0;
-        for(int i = 1; i <= k; i++) {
-            count += go(n - 1, k, target - i,dp);
-            count %= mod;
-        }
-        
-        return dp[n][target] = count;
-    }
+  
 }
