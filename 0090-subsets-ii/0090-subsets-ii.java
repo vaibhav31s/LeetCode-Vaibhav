a72d35eff14d5d1759c793b284a0c4a7e5cf69f5
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-03-09 21:12:51 +0530

    Time: 4 ms (19.56%), Space: 45 MB (5.27%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    HashSet<String> set = new HashSet<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> dynamicList = new ArrayList<>();
+        List<List<Integer>> answer = new ArrayList<>();
+        
+        go(answer, dynamicList, nums, 0, new StringBuilder());
+        
+        return answer;
+    }
+    void go(List<List<Integer>> answer, List<Integer> dynamicList, int[] nums, int index, StringBuilder sb) {
+        if (set.contains(dynamicList.toString())) return;
+        set.add(dynamicList.toString());
+        
+        answer.add(new ArrayList<>(dynamicList));
+        for (int i = index; i < nums.length; i++) {
+            dynamicList.add(nums[i]);
+            go(answer, dynamicList, nums, i + 1, sb);
+            dynamicList.remove(dynamicList.size() - 1);
+        }
+    }
+}
