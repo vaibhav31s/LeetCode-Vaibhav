ad3694a2efccc17c48e0cd2a8ce75e0e3d6d9434
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-03-11 15:04:52 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (85.81%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -3,16 +3,8 @@ class Solution {
         int[] map = new int[26];
         for (char ch : s.toCharArray()) map[ch - 'a']++;
         StringBuilder sb = new StringBuilder();
-        for (char ch : order.toCharArray()) {
-            int index = ch - 'a';
-            while (map[index]-- > 0) sb.append(ch);
-        }
-        
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            int index = ch - 'a';
-            while (map[index]-- > 0) sb.append(ch);
-        }
-        
+        for (char ch : order.toCharArray()) while (map[ch - 'a']-- > 0) sb.append(ch);
+        for (char ch = 'a'; ch <= 'z'; ch++) while (map[ch - 'a']-- > 0) sb.append(ch);
         return sb.toString();
     }
 }
