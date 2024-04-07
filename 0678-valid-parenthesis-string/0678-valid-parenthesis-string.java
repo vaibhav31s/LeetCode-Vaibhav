bba2a576412394c46b334658a49d5775752a9afd
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-04-07 23:39:25 +0530

    Time: 9 ms (5.02%), Space: 44.4 MB (5.28%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,6 +1,30 @@
 class Solution {
     public boolean checkValidString(String s) {
-         return go(s.toCharArray(), 0, 0, new Boolean[102][202]);
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        Boolean[][] dp = new Boolean[n + 1][201]; 
+        dp[n][0] = true;
+        for (int i = n; i >= 0; i--) {
+            for (int balance = 0; balance <= 200; balance++) {
+                if (i == n) {
+                    dp[i][balance] = (balance == 0);
+                } else {
+                    boolean ans = false;
+                    if (arr[i] == '*') {
+                        if (balance + 1 <= 200) ans |= dp[i + 1][balance + 1];
+                        if (balance - 1 >= 0) ans |= dp[i + 1][balance - 1];
+                        ans |= dp[i + 1][balance];
+                    } else if (arr[i] == '(') {
+                        if (balance + 1 <= 200) ans |= dp[i + 1][balance + 1];
+                    } else {
+                        if (balance - 1 >= 0) ans |= dp[i + 1][balance - 1];
+                    }
+                    dp[i][balance] = ans;
+                }
+            }
+        }
+
+        return dp[0][0]; // balance of 0 corresponds to 100 in the array
     }
    boolean go(char[] arr, int i, int balance, Boolean[][] dp) {
        
