26f65b15126a8204e624afb229080b9f60d1f326
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-04-09 12:07:53 +0530

    Time: 9 ms (30.63%), Space: 43.7 MB (20.55%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,18 +1,17 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int person = 1;
-        Deque<Pair<Integer, Integer>> q = new LinkedList<>();
-        for (int i = 0; i < tickets.length; i++) q.add(new Pair(i,tickets[i]));
+        Deque<Integer> q = new LinkedList<>();
+        for (int i = 0; i < tickets.length; i++) q.add(i);
         
         int time = 0;
         while (!q.isEmpty()) {
             time++;
-            Pair<Integer, Integer> p = q.pollFirst();
-            int val = p.getValue() - 1;
-            
-            if (val > 0) {
-                q.add(new Pair(p.getKey(), val));
-            } else if (p.getKey() == k) return time;
+            Integer first = q.pollFirst();
+            int val = --tickets[first];
+            if (val > 0) 
+                q.add(first);
+            else if (first == k) return time;
         }
         
         return time;
