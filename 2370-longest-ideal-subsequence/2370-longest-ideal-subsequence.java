4ba010ec84defc153f08aa41ee0f539945f1e4cd
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-04-28 01:30:39 +0530

    Time: 57 ms (23.52%), Space: 44.5 MB (79.36%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -3,15 +3,10 @@ class Solution {
         int[] dp = new int[26];
         for (int i = 0; i < s.length(); i++) {   
             int ind = s.charAt(i) - 'a';
-            if (dp[ind] == -1) dp[ind] = 0;
             int max = 1;
             for (char ch = 'a'; ch <= 'z'; ch++) {
                 if (Math.abs(s.charAt(i) - ch) <= k) {
-                    if (dp[ch - 'a'] == -1) {
-                        
-                    } else {
-                        max = Math.max(1 + dp[ch - 'a'], max);
-                    }
+                    max = Math.max(1 + dp[ch - 'a'], max);
                 } 
             }
             dp[ind] = max;
