98f9102977b0f8cec5b301670cbd6777d3834509
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-05-11 10:50:54 +0530

    Time: 1 ms (75.95%), Space: 49.6 MB (30.20%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -2,6 +2,7 @@ class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int start = matrix[0][0], end = matrix[matrix.length - 1][matrix.length - 1];
         int ans = start;
+        
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (check(matrix, mid, k)) {
@@ -17,14 +18,41 @@ class Solution {
     public boolean check(int[][] matrix, int mid, int k) {
         int count = 0;
         for (int[] mat : matrix) {
-            for (int x : mat) {
-                if (x <= mid) {
-                    count++;
-                }
+            int ub = Arrays.binarySearch(mat, mid);
+            
+            if (ub<0) {
+                ub=Math.abs(ub)-1;
+            } else {
+                int y= mat[ub];
+                for(int i= ub + 1; i< mat.length; i++){
+                    if (mat[i] == y) 
+                        ++ub;
+                    else 
+                        break;
+                 }
+             ++ub;
             }
+                
+            count += ub;
         }
         
         return k <= count;
+    }
+    
+    public int upperbound(int[] mat, int k) {
+        int start = 0, end = mat.length - 1;
+        int ans = -1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid > k) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                ans = mid;
+            }
+        }
+        return ans + 1;
     }
     
 }
