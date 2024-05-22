91c93dea2e168975de71e23d53a31b1afd7f62c4
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-05-23 02:46:08 +0530

    Time: 75 ms (91.42%), Space: 45 MB (98.96%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,16 +1,23 @@
 class Solution {
+    private static final int MAX_NUM = 5000001;
+    private static final  boolean[] prime = new boolean[MAX_NUM];
+    private static boolean isDone = false;
     public int countPrimes(int n) {
-        boolean[] prime = new boolean[n + 1];
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) continue;
-            else count++;
-            for (int j = i + i; j <= n; j += i) {
-                if (!prime[j]) {
-                    prime[j] = true;
+        if (!isDone) {
+            for (int i = 2; i < MAX_NUM; i++) {
+                if (prime[i]) continue;
+                for (int j = i + i; j < MAX_NUM; j += i) {
+                    if (!prime[j]) {
+                        prime[j] = true;
+                    }
                 }
-            }
+            } 
+            isDone = true;
         }
+        
+        
+        int count = 0;
+        for (int i = 2; i < n; i++) if (!prime[i]) count++; 
         return count;
     }
 }
