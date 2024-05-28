5e5c03f97ea6a6ac2b11f01ed46b75aee17dc876
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-05-29 04:22:06 +0530

    Time: 7 ms (73.63%), Space: 43.5 MB (12.35%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -2,24 +2,21 @@ class Solution {
     //pref binary slidingWindow
     public int equalSubstring(String s, String t, int maxCost) {
         int start = 0;
-        int end = s.length();
-        int answer = 0;
+        int max = 0;
         int[] pref = new int[s.length()+1];
         
         for (int i = 1; i <= s.length(); i++) {
             pref[i] = pref[i - 1] + Math.abs(s.charAt(i-1) - t.charAt(i-1));
         }
-        while (start <= end) {
-            int mid = start + (end - start)/2;
-            if (isGood(s, t, maxCost, mid, pref)) {
-                answer = mid;
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+        
+        for (int i = 0; i <= s.length(); i++) {
+            while ((pref[i] - pref[start]) > maxCost) {
+                start++;
             }
+            max = Math.max(max, i - start);
         }
         
-        return answer;
+        return max;
     }
     
     boolean isGood(String s, String t, int maxCost, int win, int[] pref) {
