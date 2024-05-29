07c1332b87d16c5369c2e4f3220d94ce38988031
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-05-29 06:05:39 +0530

    Time: 12 ms (29.60%), Space: 45.3 MB (62.74%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,37 +1,10 @@
 class Solution {
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
-        int answer = Integer.MIN_VALUE ;
-        
-        
-        for (int i = 0; i <= nums.length - 3; i++) {
-            answer = Math.max(answer, nums[i] * nums[i + 1] * nums[i + 2]);
-        }
-        
-        int zero = 0;
-        int neg = 0;
-        int pos = 0;
-        
-        for (int x : nums) {
-            if (x < 0) neg++;
-            if (x > 0) pos++;
-            if (x == 0) zero++;
-        }
-        // System.out.println(zero + " " + neg + " " + pos);
-        if (zero > nums.length - 3) return 0;
-
-        if (zero == 0 && neg == 0) {
-            return answer;
-        }
-        
-        if (zero == 0 && pos == 0) {
-            return nums[nums.length - 2] * nums[nums.length - 1] * nums[nums.length - 3];
-        }
-        int twoneg = nums[0] * nums[1] * nums[nums.length - 1];
+        int third = nums[0] * nums[1] * nums[2];
+        int forth = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+        int first = nums[0] * nums[1] * nums[nums.length - 1];
         int second = nums[nums.length -1] * nums[nums.length -2] * nums[0];
-       
-        
-        
-        return Math.max(twoneg, Math.max(answer, second));
+        return Math.max(first, Math.max(third, Math.max(forth, second)));
     }
 }
