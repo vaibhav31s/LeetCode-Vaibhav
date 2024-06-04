f60975ebf305d2764d69f5091ed518742a871bff
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-06-05 03:47:12 +0530

    Time: 0 ms (100.00%), Space: 41 MB (59.14%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
@@ -1,8 +1,11 @@
 class Solution {
     public int minCostToMoveChips(int[] position) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int x : position) map.put(x % 2, map.getOrDefault(x % 2, 0) + 1);
-        if (!map.containsKey(0) || !map.containsKey(1)) return 0;
-        return Math.min(map.get(0), map.get(1));
+        int one = 0, zero = 0;
+        for (int x : position) {
+            if (x % 2 == 0) zero++;
+            else one++;
+        }
+        if (one == 0 || zero == 0) return 0;
+        return Math.min(one, zero);
     }
 }
