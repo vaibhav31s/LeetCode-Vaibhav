3fdb5128601e40082fcb4af348a9a7a91539f077
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-08-06 10:12:04 +0530

    Time: 11 ms (46.54%), Space: 45.6 MB (64.04%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -8,11 +8,15 @@ class Solution {
         
         int ans = 0;
         PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        
         for (int x : map) heap.add(x);
+        
         int[] keyboard = new int[10];
         int curPointer = 1;
+        
         while (!heap.isEmpty()) {
             int max = heap.poll();
+            
             if (keyboard[curPointer] >= 8) {
                 curPointer++;
             } 
