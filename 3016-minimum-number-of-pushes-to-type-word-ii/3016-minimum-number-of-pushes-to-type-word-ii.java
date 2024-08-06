d0a8dc908951c26edcc7d993556d242df49af5e8
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-08-06 10:08:24 +0530

    Time: 10 ms (49.42%), Space: 45.9 MB (17.31%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] map = new int[26];
+        
+        for (char ch : word.toCharArray()) {
+            map[ch - 'a']++;
+        }
+        
+        int ans = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int x : map) heap.add(x);
+        int[] keyboard = new int[10];
+        int curPointer = 1;
+        while (!heap.isEmpty()) {
+            int max = heap.poll();
+            if (keyboard[curPointer] >= 8) {
+                curPointer++;
+            } 
+            ans += curPointer * max;
+            keyboard[curPointer]++;
+        }
+        
+        return ans;
+    }
+}
