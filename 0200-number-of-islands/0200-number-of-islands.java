36cbce3226bc3866be5a94baa4800faba6f7ff14
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-09-10 14:30:59 +0530

    Time: 6 ms (23.25%), Space: 52.8 MB (5.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,25 +1,46 @@
 class Solution {
+    int[][] directions = new int[][]{{0, 1}, {0, -1},  {-1, 0}, {1, 0}};
     public int numIslands(char[][] grid) {
-        int rows = grid.length, cols = grid[0].length, answer = 0;
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
-                    go(grid, i, j, rows, cols);
-                    answer++;
+                    BFS(grid, i, j);
+                    count++;
                 }
             }
         }
-        return answer;
+        
+        return count;
     }
     
-    void go(char[][] grid, int row, int col, int rows, int cols) {
-        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == '0') return;
+    void BFS(char[][] grid, int i, int j) {
+        Deque<int[]> dq = new LinkedList<>();
+        dq.add(new int[]{i, j});
         
-        grid[row][col] = '0'; // Mark as visited
-        go(grid, row + 1, col, rows, cols);
-        go(grid, row, col -1, rows, cols);
-        go(grid, row - 1, col, rows, cols);
-        go(grid, row, col + 1, rows, cols);
+        while (!dq.isEmpty()) {
+            int size = dq.size();
+            while (size-- > 0) {
+                int[] coord = dq.poll();
+                for (int[] dir : directions) {
+                    int x = dir[0] + coord[0];
+                    int y = dir[1] + coord[1];
+                    if (isValidCoordinate(grid, x, y, grid.length, grid[0].length)) {
+                        dq.add(new int[]{x, y});
+                        grid[x][y] = '0';
+                    }
+                }
+            }
+        }
+    }
+    
+    boolean isValidCoordinate(char[][] grid, int x, int y, int n, int m) {
+        if (x < 0 || y < 0 || x >= n || y >= m || grid[x][y] == '0') {
+            return false;
+        }
+        
+        return true;
     }
 }
