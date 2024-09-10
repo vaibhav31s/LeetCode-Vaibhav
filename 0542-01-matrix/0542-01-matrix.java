1557c4840f45ba669bbab7af7cab472447146a65
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-09-10 13:49:10 +0530

    Time: 13 ms (82.45%), Space: 47.3 MB (51.10%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,49 +1,41 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+        Deque<int[]> dq = new LinkedList<>();
         int n = mat.length, m = mat[0].length;
-        int[][] ans = new int[n][m];
-        Deque<Pair<Integer, Integer>> dq = new LinkedList<>();
-        
-        
-        for (int[] x : ans) Arrays.fill(x, -1);
-        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
-                    dq.add(new Pair<>(i, j));
-                    ans[i][j] = 0;
-                } 
+                    dq.add(new int[]{i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
             }
         }
         
-        int[][] direction = new int[][]{{0, 1}, {0, -1}, {1, 0},  {-1, 0}};
-        
+        int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while (!dq.isEmpty()) {
             int size = dq.size();
             while (size-- > 0) {
-                Pair<Integer, Integer> lastPair = dq.poll();
-                int x = lastPair.getKey();
-                int y = lastPair.getValue();
-                
-                for (int[] curDir : direction) {
-                    int xx = x + curDir[0];
-                    int yy = y + curDir[1];
-                    if (validCoord(xx, yy, n, m, ans)) {
-                        ans[xx][yy] = 1 + ans[x][y];
-                        dq.add(new Pair<>(xx, yy));
+                int[] curCoord = dq.poll();
+                for (int[] dir : directions) {
+                    int x = curCoord[0] + dir[0];
+                    int y = curCoord[1] + dir[1];
+                    if (isValidCoord(x, y, n, m, mat)) {
+                        dq.add(new int[]{x, y});
+                        mat[x][y] = 1 + mat[curCoord[0]][curCoord[1]];
                     }
                 }
             }
-                    
         }
-        return ans;
+        
+        return mat;
     }
-                    
-    boolean validCoord(int x, int y, int n, int m, int[][] ans) {
-        if (x < 0 || y < 0 || x >= n || y >= m || ans[x][y] != -1) return false;
+    
+    boolean isValidCoord(int x, int y, int n, int m, int[][] mat) {
+        if (x < 0 || y < 0 || x >= n || y >= m || mat[x][y] != -1) {
+            return false;
+        }
         
         return true;
     }
-    
-    
 }
