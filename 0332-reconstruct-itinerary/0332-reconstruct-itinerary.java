8013875d963f5e81dbd2543379f02d29cb09b0e2
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-09-11 16:23:18 +0530

    Time: 6 ms (69.44%), Space: 45.7 MB (10.19%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -0,0 +1,38 @@
+class Solution {
+    int totalTickets = 0;
+    boolean isAnswerFound = false;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        totalTickets = tickets.size();
+      
+        
+        HashMap<String, List<String>> alist = new HashMap<>();
+        
+        for (List<String> edge : tickets) {
+            String A = edge.get(0);
+            String B = edge.get(1);
+            //Since its A to B
+            alist.putIfAbsent(A, new ArrayList<>());
+            alist.get(A).add(B);
+        }
+        
+        for (List<String> ma : alist.values()) {
+            Collections.sort(ma);
+        }
+        List<String> answer = new ArrayList<>();
+        
+        travel(alist, "JFK", answer);
+        return answer;
+    }
+    
+    void travel(HashMap<String, List<String>> map, String from, List<String> answer) {
+        List<String> neig = map.getOrDefault(from, new ArrayList<>());
+        while(!neig.isEmpty()){
+            String to = neig.remove(0);
+            travel(map, to, answer);
+        }
+        answer.add(0,from);
+    }
+        
+       
+    
+}
