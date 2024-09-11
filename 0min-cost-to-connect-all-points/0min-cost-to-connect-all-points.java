3b00148a220044521af5f7d3f08825b18a9e16c8
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-09-11 23:03:11 +0530

    Time: 1284 ms, Memory: 115 MB - LeetHub

--- 0min-cost-to-connect-all-points/0min-cost-to-connect-all-points.java
+++ 0min-cost-to-connect-all-points/0min-cost-to-connect-all-points.java
@@ -2,7 +2,6 @@ class Solution {
     public int minCostConnectPoints(int[][] points) {
         List<int[]> edges = new ArrayList<>();
         int n = points.length;
-        UnionFind uf = new UnionFind(n);
         
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -10,60 +9,54 @@ class Solution {
                 edges.add(new int[]{i, j, dist});
             }
         }
-        Collections.sort(edges, (a, b) -> Integer.compare(a[2], b[2]));
-        for (int[] edge : edges) {
-            uf.union(edge[0], edge[1], edge[2]);
+        
+        List<List<int[]>> alist = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            alist.add(new ArrayList<>());
         }
-        return uf.getSum();
+        
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1], weight = edge[2];
+            alist.get(a).add(new int[]{b, weight});
+            alist.get(b).add(new int[]{a, weight});
+        }
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.add(new int[]{0 ,0});
+        boolean[] vis = new boolean[n + 1];
+        
+        int minSpanningSum = 0;
+        while (!pq.isEmpty()) {
+            int[] curMinPoint = pq.poll();
+            int curPoint = curMinPoint[1];
+            int weight = curMinPoint[0];
+            
+            if (vis[curPoint]) continue;
+            
+            vis[curPoint] = true;
+            minSpanningSum += weight;
+            
+            for (int[] children : alist.get(curPoint)) {
+                int childrenWeight = children[1];
+                int childrenValue = children[0];
+                pq.add(new int[]{childrenWeight, childrenValue});
+            }
+            
+        }
+        
+        
+        return minSpanningSum;
     }
+    
+    
+    
     
     int manhattanDistance(int[] a, int[] b) {
         return Math.abs(a[0] - b[0]) + 
                              Math.abs(a[1] - b[1]);
     }
     
-    class UnionFind {
-        int[] root;
-        int[] rank;
-        int sum = 0;
-        UnionFind(int size) {
-            root = new int[size];
-            rank = new int[size];
-            for (int i = 0; i < size; i++) {
-                root[i] = i;
-                rank[i] = 1;
-            }
-        }
-        
-        int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
-            return root[x] = find(root[x]);
-        }
-        
-        void union(int x, int y, int weight) {
-            int rootX = find(x);
-            int rootY = find(y);
-            
-            if (rootX != rootY) {
-                if (rank[rootX] < rank[rootY]) {
-                    root[rootX] = rootY;
-                } else if (rank[rootY] < rank[rootX]) {
-                    root[rootY] = rootX;
-                } else {
-                    root[rootY] = rootX;
-                    rank[rootX] += 1;
-                }
-                
-            sum += weight;
-            }
-        }
-        
-        int getSum() {
-            return sum;
-        }
-        
-    }
+    
    
 }
