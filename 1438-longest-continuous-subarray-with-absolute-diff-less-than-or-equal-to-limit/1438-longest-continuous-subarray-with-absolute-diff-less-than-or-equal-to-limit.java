2500572393f09f2c95fb252a5032e279075840bf
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-09-13 02:21:46 +0530

    Time: 144 ms (11.50%), Space: 59.3 MB (22.86%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,42 +1,31 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        int start = 1;
-        int end = nums.length;
-        int max = 0;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (isGoodSubArray(nums, mid, limit)) {
-                start = mid + 1;
-                max = mid;
-            } else {
-                end = mid - 1;
+        int start = 0;
+        int end = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int max = 1;
+        while (start <= end && end < nums.length) {
+            if (start == end) {
+                map.put(nums[start], map.getOrDefault(nums[start], 0) + 1);
+                end++;
+            } else if (map.lastKey() - map.firstKey() <= limit) {
+                max = Math.max(max, end - start);
+                map.put(nums[end], map.getOrDefault(nums[end], 0) + 1);
+                end++;
+            }  else {
+                map.put(nums[start], map.getOrDefault(nums[start], 0) - 1);
+                if (map.get(nums[start]) == 0) {
+                    map.remove(nums[start]);
+                }
+                start++;
+            }
+            if (map.lastKey() - map.firstKey() <= limit) {
+                max = Math.max(max, end - start);
             }
         }
+        
         return max;
     }
     
-    boolean isGoodSubArray(int[] nums, int mid, int limit) {
-        TreeMap<Integer, Integer> tMap = new TreeMap<>();
-        
-        for (int i = 0; i < mid; i++) {
-            tMap.put(nums[i], tMap.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        
-       
-        
-        for (int i = 0; i < nums.length - mid; i++) {
-            int lower = tMap.firstKey();
-            int higher = tMap.lastKey();
-            if (Math.abs(higher - lower) <= limit) return true;
-            tMap.put(nums[i], tMap.getOrDefault(nums[i], 0) - 1);
-            if (tMap.get(nums[i]) == 0) tMap.remove(nums[i]);
-            tMap.put(nums[i + mid], tMap.getOrDefault(nums[i + mid], 0) + 1);
-        }
-        int lower = tMap.firstKey();
-        int higher = tMap.lastKey();
-        if (Math.abs(higher - lower) <= limit) return true;
-        
-        return false;
-    }
+   
 }
