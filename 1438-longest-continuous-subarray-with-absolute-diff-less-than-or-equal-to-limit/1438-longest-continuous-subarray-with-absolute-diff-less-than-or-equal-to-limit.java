9a1c8842963a76c5506a31bfc57c5a36353732e1
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2024-09-13 02:15:25 +0530

    Time: 1918 ms (5.25%), Space: 61 MB (6.25%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int longestSubarray(int[] nums, int limit) {
+        int start = 0;
+        int end = nums.length;
+        int max = 0;
+        if (nums.length == 1) return 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isGoodSubArray(nums, mid, limit)) {
+                start = mid + 1;
+                max = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return max;
+    }
+    
+    boolean isGoodSubArray(int[] nums, int mid, int limit) {
+        TreeMap<Integer, Integer> tMap = new TreeMap<>();
+        
+        for (int i = 0; i < mid; i++) {
+            tMap.put(nums[i], tMap.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        
+       
+        
+        for (int i = 0; i < nums.length - mid; i++) {
+            int lower = tMap.firstKey();
+            int higher = tMap.lastKey();
+            if (Math.abs(higher - lower) <= limit) return true;
+            tMap.put(nums[i], tMap.getOrDefault(nums[i], 0) - 1);
+            if (tMap.get(nums[i]) == 0) tMap.remove(nums[i]);
+            tMap.put(nums[i + mid], tMap.getOrDefault(nums[i + mid], 0) + 1);
+        }
+        int lower = tMap.firstKey();
+        int higher = tMap.lastKey();
+        if (Math.abs(higher - lower) <= limit) return true;
+        
+        return false;
+    }
+}
